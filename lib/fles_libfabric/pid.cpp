773630ba2dde3f947559ccad9b52c186564695a4
fsalem <farouqsalem@gmail.com>
2017-05-17 16:53:08 +0200

    limit the PID integral sum

--- lib/fles_libfabric/pid.cpp
+++ lib/fles_libfabric/pid.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <cmath>
+#include <vector>
 #include "pid.h"
 
 using namespace std;
@@ -26,6 +27,8 @@ class PIDImpl
         double _Ki;
         uint64_t _pre_error;
         double _integral;
+        vector<uint64_t> integral_vals;
+        int next_indx=0;
 };
 
 
@@ -59,6 +62,7 @@ PIDImpl::PIDImpl( double dt, uint64_t max, uint64_t min, double Kp, double Kd, d
     _pre_error(0),
     _integral(0)
 {
+	integral_vals.resize(10000,0);
 }
 
 uint64_t PIDImpl::calculate( uint64_t setpoint, uint64_t pv )
@@ -66,12 +70,21 @@ uint64_t PIDImpl::calculate( uint64_t setpoint, uint64_t pv )
     
     // Calculate error
 	uint64_t error = setpoint - pv;
+	integral_vals[next_indx] = error;
 
     // Proportional term
     double Pout = _Kp * error;
 
     // Integral term
     _integral += error * _dt;
+
+    if (next_indx - 1 <= 0)
+    	_integral -= integral_vals[integral_vals.size()-1] * _dt;
+    else
+    	_integral -= integral_vals[next_indx-1] * _dt;
+
+    next_indx = (next_indx+1)%integral_vals.size();
+
     double Iout = _Ki * _integral;
 
     // Derivative term
