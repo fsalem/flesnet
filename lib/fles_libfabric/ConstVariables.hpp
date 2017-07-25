3f936595091763c2a595db4617c25a10d36ff449
fsalem <farouqsalem@gmail.com>
2017-07-25 19:07:05 +0200

    Const variables file

---
+++ lib/fles_libfabric/ConstVariables.hpp
@@ -0,0 +1,16 @@
+// Copyright 2012-2013 Jan de Cuveland <cmail@cuveland.de>
+// Copyright 2016 Thorsten Schuett <schuett@zib.de>, Farouk Salem <salem@zib.de>
+
+#pragma once
+
+#include <iostream>
+
+#pragma pack(1)
+
+namespace tl_libfabric
+{
+
+const static uint64_t MINUS_ONE = -1;
+const static uint64_t ZERO = 0;
+}
+#pragma pack()
