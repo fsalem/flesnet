c859dfd88f71664947d25244d1c7bb5d69b2c856
Farouk Salem <salem@zib.de>
2018-04-04 15:30:34 +0200

    add MPI dependencies

---
+++ cmake/FindMPI.cmake
@@ -0,0 +1,16 @@
+# Copyright 2016 Thorsten Schuett <schuett@zib.de>, Farouk Salem <salem@zib.de>
+
+find_path(MPI_INCLUDE_DIR)
+find_library(MPI_LIBRARY)
+
+MESSAGE(STATUS "MPI           = ${MPI_LIBRARY}")
+MESSAGE(STATUS "MPI           = ${MPI_INCLUDE_DIR}")
+
+SET(CMAKE_C_COMPILER mpicc)
+SET(CMAKE_CXX_COMPILER mpicxx)
+
+include_directories(SYSTEM ${MPI_INCLUDE_DIR})
+
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(MPI REQUIRED_VARS MPI_LIBRARY MPI_INCLUDE_DIR)
