f8071f61e19c1a6c99efbd3920f715e3844fa6a9
bzcsalem <bzcsalem@das-pc1.zib.de>
2019-08-06 12:33:26 +0200

    Context Pool implementation

---
+++ lib/fles_libfabric/LibfabricContextPool.cpp
@@ -0,0 +1,48 @@
+// Copyright 2019 Farouk Salem <salem@zib.de>
+
+#include "LibfabricContextPool.hpp"
+
+namespace tl_libfabric
+{
+
+
+std::unique_ptr<LibfabricContextPool>& LibfabricContextPool::getInst() {
+    if (LibfabricContextPool::context_pool_ == nullptr)
+	LibfabricContextPool::context_pool_ = std::unique_ptr<LibfabricContextPool>(new LibfabricContextPool());
+
+    return LibfabricContextPool::context_pool_;
+}
+
+LibfabricContextPool::LibfabricContextPool() {
+    ///
+}
+LibfabricContextPool::~LibfabricContextPool(){
+    available_.clear();
+    in_use_.clear();
+    LibfabricContextPool::context_pool_ = nullptr;
+    L_(info) << "LibfabricContextPool deconstructor: Total number of created objects " << context_counter_;
+}
+
+struct fi_custom_context* LibfabricContextPool::getContext() {
+    pool_mutex_.lock();
+    struct fi_custom_context context;
+    if (available_.empty()){
+	context.id = context_counter_++;
+
+    }else{
+	struct fi_custom_context context = available_[0];
+	available_.erase(available_.begin());
+    }
+    pool_mutex_.unlock();
+    in_use_.push_back(context);
+    return &context;
+}
+
+void LibfabricContextPool::releaseContext(struct fi_custom_context* context) {
+    L_(debug) << "LibfabricContextPool: Context released with ID " << context->id;
+    available_.push_back(*context);
+}
+
+std::unique_ptr<LibfabricContextPool> LibfabricContextPool::context_pool_ = nullptr;
+
+}
---
+++ lib/fles_libfabric/LibfabricContextPool.hpp
@@ -0,0 +1,47 @@
+// Copyright 2019 Farouk Salem <salem@zib.de>
+
+/**
+ * An implementation of fi_context object pool based on the Object Pool Design Pattern
+ */
+#pragma once
+
+#include <log.hpp>
+#include <memory>
+#include <rdma/fabric.h>
+#include <vector>
+#include <string.h>
+#include <mutex>
+
+namespace tl_libfabric
+{
+struct fi_custom_context{
+	struct fi_context context;
+	uint64_t id;
+};
+
+class LibfabricContextPool
+{
+public:
+    ~LibfabricContextPool();
+
+    LibfabricContextPool(const LibfabricContextPool&) = delete;
+    LibfabricContextPool& operator=(const LibfabricContextPool&) = delete;
+
+    struct fi_custom_context* getContext();
+
+    void releaseContext(struct fi_custom_context* context);
+
+    static std::unique_ptr<LibfabricContextPool>& getInst();
+
+private:
+    static std::unique_ptr<LibfabricContextPool> context_pool_;
+    std::vector<struct fi_custom_context> available_;
+    std::vector<struct fi_custom_context> in_use_;
+
+    uint64_t context_counter_ = 0;
+
+    std::mutex pool_mutex_;
+
+    LibfabricContextPool();
+};
+}
