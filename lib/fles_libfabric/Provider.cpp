8125ca3ce3789836dfb94999f7847ecbd8e6d441
bzcsalem <bzcsalem@das-pc1.zib.de>
2019-08-08 15:24:46 +0200

    updates in logs

--- lib/fles_libfabric/Provider.cpp
+++ lib/fles_libfabric/Provider.cpp
@@ -89,7 +89,7 @@ struct fi_info* Provider::get_hints(enum fi_ep_type ep_type, std::string prov)
     return hints;
 }
 
-void dump_fi_info(const struct fi_info *info) {
+void Provider::dump_fi_info(const struct fi_info *info) {
     fprintf(stdout, "\nSTART dumping a fi_info object ...\n");
     int count = 0;
     while (info != NULL){
@@ -121,10 +121,10 @@ void dump_fi_info(const struct fi_info *info) {
 	fprintf(stdout, "\t\t rx_attr->total_buffered_recv %zd\n",info->rx_attr->total_buffered_recv);
 	/*/////////////// END rx_attr /////////////////////////////// */
 
-	fprintf(stdout, "\t addr_format %du\n",info->addr_format);
+	fprintf(stdout, "\t addr_format %d\n",info->addr_format);
 
 	/*/////////////// ep_attr ////////////////////////////// */
-	fprintf(stdout, "\t\t ep_attr->auth_key %du\n",info->ep_attr->auth_key);
+	fprintf(stdout, "\t\t ep_attr->auth_key %d\n",info->ep_attr->auth_key);
 	fprintf(stdout, "\t\t ep_attr->auth_key_size %zd\n",info->ep_attr->auth_key_size);
 	fprintf(stdout, "\t\t ep_attr->max_msg_size %zd\n",info->ep_attr->max_msg_size);
 	fprintf(stdout, "\t\t ep_attr->max_order_raw_size %zd\n",info->ep_attr->max_order_raw_size);
@@ -132,11 +132,11 @@ void dump_fi_info(const struct fi_info *info) {
 	fprintf(stdout, "\t\t ep_attr->max_order_waw_size %zd\n",info->ep_attr->max_order_waw_size);
 	fprintf(stdout, "\t\t ep_attr->mem_tag_format %llu\n",info->ep_attr->mem_tag_format);
 	fprintf(stdout, "\t\t ep_attr->msg_prefix_size %zd\n",info->ep_attr->msg_prefix_size);
-	fprintf(stdout, "\t\t ep_attr->protocol %du\n",info->ep_attr->protocol);
-	fprintf(stdout, "\t\t ep_attr->protocol_version %du\n",info->ep_attr->protocol_version);
+	fprintf(stdout, "\t\t ep_attr->protocol %d\n",info->ep_attr->protocol);
+	fprintf(stdout, "\t\t ep_attr->protocol_version %d\n",info->ep_attr->protocol_version);
 	fprintf(stdout, "\t\t ep_attr->rx_ctx_cnt %zd\n",info->ep_attr->rx_ctx_cnt);
 	fprintf(stdout, "\t\t ep_attr->tx_ctx_cnt %zd\n",info->ep_attr->tx_ctx_cnt);
-	fprintf(stdout, "\t\t ep_attr->type %du\n",info->ep_attr->type);
+	fprintf(stdout, "\t\t ep_attr->type %d\n",info->ep_attr->type);
 	/*/////////////// END ep_attr /////////////////////////////// */
 
 	fprintf(stdout, "\t src_addrlen %zd\n",info->src_addrlen);
@@ -144,13 +144,13 @@ void dump_fi_info(const struct fi_info *info) {
 	/*/////////////// domain_attr ////////////////////////////// */
 	fprintf(stdout, "\t\t domain_attr->auth_key %llu\n",info->domain_attr->auth_key);
 	fprintf(stdout, "\t\t domain_attr->auth_key_size %zd\n",info->domain_attr->auth_key_size);
-	fprintf(stdout, "\t\t domain_attr->av_type %du\n",info->domain_attr->av_type);
+	fprintf(stdout, "\t\t domain_attr->av_type %d\n",info->domain_attr->av_type);
 	fprintf(stdout, "\t\t domain_attr->caps %llu\n",info->domain_attr->caps);
 	fprintf(stdout, "\t\t domain_attr->cntr_cnt %zd\n",info->domain_attr->cntr_cnt);
-	fprintf(stdout, "\t\t domain_attr->control_progress %du\n",info->domain_attr->control_progress);
+	fprintf(stdout, "\t\t domain_attr->control_progress %d\n",info->domain_attr->control_progress);
 	fprintf(stdout, "\t\t domain_attr->cq_cnt %zd\n",info->domain_attr->cq_cnt);
 	fprintf(stdout, "\t\t domain_attr->cq_data_size %zd\n",info->domain_attr->cq_data_size);
-	fprintf(stdout, "\t\t domain_attr->data_progress %du\n",info->domain_attr->data_progress);
+	fprintf(stdout, "\t\t domain_attr->data_progress %d\n",info->domain_attr->data_progress);
 	fprintf(stdout, "\t\t domain_attr->ep_cnt %zd\n",info->domain_attr->ep_cnt);
 	fprintf(stdout, "\t\t domain_attr->max_ep_rx_ctx %zd\n",info->domain_attr->max_ep_rx_ctx);
 	fprintf(stdout, "\t\t domain_attr->max_ep_srx_ctx %zd\n",info->domain_attr->max_ep_srx_ctx);
@@ -163,19 +163,19 @@ void dump_fi_info(const struct fi_info *info) {
 	fprintf(stdout, "\t\t domain_attr->mr_key_size %zd\n",info->domain_attr->mr_key_size);
 	fprintf(stdout, "\t\t domain_attr->mr_mode %d\n",info->domain_attr->mr_mode);
 	fprintf(stdout, "\t\t domain_attr->name %s\n",info->domain_attr->name);
-	fprintf(stdout, "\t\t domain_attr->resource_mgmt %du\n",info->domain_attr->resource_mgmt);
+	fprintf(stdout, "\t\t domain_attr->resource_mgmt %d\n",info->domain_attr->resource_mgmt);
 	fprintf(stdout, "\t\t domain_attr->rx_ctx_cnt %zd\n",info->domain_attr->rx_ctx_cnt);
-	fprintf(stdout, "\t\t domain_attr->threading %du\n",info->domain_attr->threading);
+	fprintf(stdout, "\t\t domain_attr->threading %d\n",info->domain_attr->threading);
 	fprintf(stdout, "\t\t domain_attr->tx_ctx_cnt %zd\n",info->domain_attr->tx_ctx_cnt);
 	/*/////////////// END domain_attr /////////////////////////////// */
 
 	fprintf(stdout, "\t dest_addrlen %zd\n",info->dest_addrlen);
 
 	/*/////////////// fabric_attr ////////////////////////////// */
-	fprintf(stdout, "\t\t fabric_attr->api_version %du\n",info->fabric_attr->api_version);
+	fprintf(stdout, "\t\t fabric_attr->api_version %d\n",info->fabric_attr->api_version);
 	fprintf(stdout, "\t\t fabric_attr->name %s\n",info->fabric_attr->name);
 	fprintf(stdout, "\t\t fabric_attr->prov_name %s\n",info->fabric_attr->prov_name);
-	fprintf(stdout, "\t\t fabric_attr->prov_version %du\n",info->fabric_attr->prov_version);
+	fprintf(stdout, "\t\t fabric_attr->prov_version %d\n",info->fabric_attr->prov_version);
 	/*/////////////// END fabric_attr /////////////////////////////// */
 
 	info = info->next;
