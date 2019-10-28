5127c16c17c1d670d5620600077667ed96b63d8a
bzcsalem <bzcsalem@farouks-mbp.fritz.box>
2019-10-28 15:48:03 +0100

    Increase log size and add timeout temp variable

--- lib/fles_libfabric/ConstVariables.hpp
+++ lib/fles_libfabric/ConstVariables.hpp
@@ -12,9 +12,11 @@ struct ConstVariables {
     const static uint64_t ZERO = 0;
     const static uint32_t ONE_HUNDRED = 100;
 
-    const static uint32_t MAX_HISTORY_SIZE = 20000; // high for logging ... should be small ~100-200
+    const static uint32_t MAX_HISTORY_SIZE = 500000; // high for logging ... should be small ~100-200
 
     const static uint16_t MAX_DESCRIPTOR_ARRAY_SIZE = 10;
+
+    constexpr static double TIMEOUT = 2.0; // in seconds
 
     ///-----
     const static uint32_t MAX_MEDIAN_VALUES = 10;
