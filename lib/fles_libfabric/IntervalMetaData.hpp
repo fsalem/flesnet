55f70eab380dce59bcf2fd4df51e5a3f5cbc8cdc
bzcsalem <bzcsalem@das-pc1.zib.de>
2020-01-12 14:12:42 +0100

    very minor changes

--- lib/fles_libfabric/IntervalMetaData.hpp
+++ lib/fles_libfabric/IntervalMetaData.hpp
@@ -14,18 +14,24 @@ namespace tl_libfabric
 struct IntervalMetaData {
     /// The interval index
     uint64_t interval_index = ConstVariables::MINUS_ONE;
+
     /// The interval's rounds
     uint32_t round_count = 1;
+
     /// Start timeslice
     uint64_t start_timeslice = ConstVariables::MINUS_ONE;
+
+    /// Last timeslice
     uint64_t last_timeslice = ConstVariables::MINUS_ONE;
 
     // The start time for the interval [The actual time when a input node is the sender, the proposed time when a compute node is the sender]
     std::chrono::high_resolution_clock::time_point start_time;
+
     // duration for the whole interval
     uint64_t interval_duration;
 
     IntervalMetaData(){}
+    
     IntervalMetaData(uint64_t index, uint32_t rounds, uint64_t start_ts, uint64_t last_ts,
 		    std::chrono::high_resolution_clock::time_point start_time,
 		    uint64_t duration):
