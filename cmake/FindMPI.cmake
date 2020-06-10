1529ae5a29b978a2a24bc6ade464ebce491dfad0
Farouk Salem <salem@zib.de>
2020-06-10 18:40:04 +0200

    update findMPI

--- cmake/FindMPI.cmake
+++ cmake/FindMPI.cmake
@@ -1,16 +1,18 @@
 # Copyright 2016 Thorsten Schuett <schuett@zib.de>, Farouk Salem <salem@zib.de>
 
-find_path(MPI_INCLUDE_DIR)
-find_library(MPI_LIBRARY)
+find_package(MPI REQUIRED)
+if (MPI_FOUND)
+	MESSAGE(STATUS "MPI           = ${MPI_LIBRARY}")
+	MESSAGE(STATUS "MPI           = ${MPI_INCLUDE_DIR}")
 
-MESSAGE(STATUS "MPI           = ${MPI_LIBRARY}")
-MESSAGE(STATUS "MPI           = ${MPI_INCLUDE_DIR}")
+	SET(CMAKE_C_COMPILER mpicc)
+	SET(CMAKE_CXX_COMPILER mpicxx)
 
-SET(CMAKE_C_COMPILER mpicc)
-SET(CMAKE_CXX_COMPILER mpicxx)
+	include_directories(SYSTEM ${MPI_INCLUDE_DIR})
+	add_definitions(-DOMPI_SKIP_MPICXX)
 
-include_directories(SYSTEM ${MPI_INCLUDE_DIR})
-add_definitions(-DOMPI_SKIP_MPICXX)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MPI REQUIRED_VARS MPI_LIBRARY MPI_INCLUDE_DIR)
+	include(FindPackageHandleStandardArgs)
+	find_package_handle_standard_args(MPI REQUIRED_VARS MPI_LIBRARY MPI_INCLUDE_DIR)
+else (MPI_FOUND)
+    message(SEND_ERROR "This application cannot compile without MPI")
+endif (MPI_FOUND)
