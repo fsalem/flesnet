32ff435bbee68b9881495392870aafa67fb6d81b
Farouk Salem <salem@zib.de>
2020-06-10 18:56:18 +0200

    add back FindMPI

---
+++ cmake/FindMPI.cmake
@@ -0,0 +1,16 @@
+# Copyright 2016 Thorsten Schuett <schuett@zib.de>, Farouk Salem <salem@zib.de>
+
+find_path(MPI_INCLUDE_DIR mpi.h)
+find_library(MPI_LIBRARY NAMES mpi)
+
+MESSAGE(STATUS "MPI           = ${MPI_LIBRARY}")
+MESSAGE(STATUS "MPI           = ${MPI_INCLUDE_DIR}")
+
+SET(CMAKE_C_COMPILER mpicc)
+SET(CMAKE_CXX_COMPILER mpicxx)
+
+include_directories(SYSTEM ${MPI_INCLUDE_DIR})
+add_definitions(-DOMPI_SKIP_MPICXX)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(MPI REQUIRED_VARS MPI_LIBRARY MPI_INCLUDE_DIR)
